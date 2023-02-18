71d012063d6243331d9f8cc476ed6af1ecec8d65
Trajy <henrique.trajano.a@gmail.com>
2023-02-18 10:50:08 -0300

    created-controller-abstract

---
+++ src/main/java/br/com/trajy/architecture/controller/ControllerAbstract.java
@@ -0,0 +1,13 @@
+package br.com.trajy.architecture.controller;
+
+import br.com.trajy.architecture.controller.config.ControllerConfigAbstract;
+import br.com.trajy.architecture.resource.AuditableResource;
+
+public abstract class ControllerAbstract<ID_TYPE, RESOURCE extends AuditableResource<ID_TYPE>>
+        implements FindController<ID_TYPE, RESOURCE>, SaveController<ID_TYPE, RESOURCE>,
+        UpdateController<ID_TYPE, RESOURCE>, DeleteController<ID_TYPE> {
+
+    @Override
+    public abstract <CONFIG extends ControllerConfigAbstract> CONFIG getConfig();
+
+}
