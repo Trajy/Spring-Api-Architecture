e030ddb2e60f9d34235af535aa21708b3c35350d
Trajy <henrique.trajano.a@gmail.com>
2023-02-18 16:15:54 -0300

    Revert "Update AssemblyMapperInterface.java"
    This reverts commit 6054a73d654af8e9e559ebb33abd8f499e3c3084.

--- src/main/java/br/com/trajy/architecture/assembly/AssemblyMapperInterface.java
+++ src/main/java/br/com/trajy/architecture/assembly/AssemblyMapperInterface.java
@@ -1,9 +1,7 @@
 package br.com.trajy.architecture.assembly;
 
-import org.mapstruct.Mapper;
 import java.util.List;
 
-@Mapper
 public interface AssemblyMapperInterface<RESOURCE, ENTITY> {
 
     RESOURCE toResource(ENTITY entity);
