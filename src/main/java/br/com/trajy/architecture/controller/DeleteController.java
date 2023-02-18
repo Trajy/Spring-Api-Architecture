0426a6f74ebcfce1970d44d4d8d0feaa3c40d907
Trajy <henrique.trajano.a@gmail.com>
2023-02-18 04:29:38 -0300

    complete-implamantation-DeleteController

--- src/main/java/br/com/trajy/architecture/controller/DeleteController.java
+++ src/main/java/br/com/trajy/architecture/controller/DeleteController.java
@@ -1,12 +1,9 @@
 package br.com.trajy.architecture.controller;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.http.ResponseEntity.noContent;
-import static org.springframework.transaction.annotation.Propagation.SUPPORTS;
 
 import br.com.trajy.architecture.controller.config.ControllerConfigAbstract;
 import br.com.trajy.architecture.model.AuditableEntity;
-import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpRequest;
@@ -15,34 +12,26 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-public interface DeleteController<RESOURCE> {
+public interface DeleteController {
 
     Logger log = LoggerFactory.getLogger(DeleteController.class);
 
     <CONFIG extends ControllerConfigAbstract> CONFIG getConfig();
 
-    @DeleteMapping(value = "/{id}", consumes = APPLICATION_JSON_VALUE)
-    @Transactional(propagation = SUPPORTS, rollbackFor = Exception.class)
-    default <ID_TYPE> ResponseEntity<Void> delete(@PathVariable String id, RESOURCE resource, HttpRequest request) {
-        log.info("DELETE | Iniciado | Controller: {} | Entity: {}", this.getClass().getSimpleName(), resource);
-        beforeDelete(resource, request);
-        // TODO - implementar findById na service
-        AuditableEntity<Object> entity = getConfig().getAssembly().toEntity(resource);
-        setAuditData(entity);
-        // TODO - implementar update
-        getConfig().getService().update(entity);
-        afterUpdate(resource, request);
+    @DeleteMapping(value = "/{id}")
+    @Transactional(rollbackFor = Exception.class)
+    default <ID_TYPE> ResponseEntity<Void> delete(@PathVariable ID_TYPE id, HttpRequest request) {
+        log.info("DELETE | Iniciado | Controller: {}", this.getClass().getSimpleName());
+        beforeDelete(request);
+        AuditableEntity<Object> entity = getConfig().getService().findById(id);
+        getConfig().getService().delete(entity);
+        afterUpdate(request);
         log.info("DELETE | Finalizado | Controller: {}", this.getClass().getSimpleName());
         return noContent().build();
     }
 
-    default void beforeDelete(RESOURCE resource, HttpRequest request) { }
+    default void beforeDelete(HttpRequest request) { }
 
-    default void afterUpdate(RESOURCE resource, HttpRequest request) { }
-
-    private <ID_TYPE> void setAuditData(AuditableEntity<ID_TYPE> entity) {
-        entity.setModifiedBy("implementar Obtencao de Loguin");
-        entity.setModifiedAt(new DateTime());
-    }
+    default void afterUpdate(HttpRequest request) { }
 
 }
