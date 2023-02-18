49dd7e4b715544af9b71f86e8267926f65898f27
Trajy <henrique.trajano.a@gmail.com>
2023-02-18 15:56:27 -0300

    changed-get-assembly-to-return-interface

--- src/main/java/br/com/trajy/architecture/controller/config/ControllerConfigInteface.java
+++ src/main/java/br/com/trajy/architecture/controller/config/ControllerConfigInteface.java
@@ -1,6 +1,6 @@
 package br.com.trajy.architecture.controller.config;
 
-import br.com.trajy.architecture.assembly.AssemblyMapperAbstract;
+import br.com.trajy.architecture.assembly.AssemblyMapperInterface;
 import br.com.trajy.architecture.model.AuditableEntity;
 import br.com.trajy.architecture.service.ServiceAbstract;
 
@@ -10,6 +10,6 @@ public interface ControllerConfigInteface {
             SERVICE extends ServiceAbstract<ID_TYPE, ENTITY>> SERVICE getService();
 
     <ID_TYPE, RESOURCE, ENTITY extends AuditableEntity<ID_TYPE>,
-            ASSEMBLY extends AssemblyMapperAbstract<RESOURCE, ENTITY>> ASSEMBLY getAssembly();
+            ASSEMBLY extends AssemblyMapperInterface<RESOURCE, ENTITY>> ASSEMBLY getAssembly();
 
 }
