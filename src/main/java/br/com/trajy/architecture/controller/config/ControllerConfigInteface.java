be0af5864069548dc58f5748b29d456974e8af10
Trajy <henrique.trajano.a@gmail.com>
2023-02-18 17:58:16 -0300

    Revert "changed-generic-resource-on-get-Assembly-to-extends-AuditableResource"
    This reverts commit 0bbf8f16ee7abdc5d6a91f5a6d85250ca9686afc.

--- src/main/java/br/com/trajy/architecture/controller/config/ControllerConfigInteface.java
+++ src/main/java/br/com/trajy/architecture/controller/config/ControllerConfigInteface.java
@@ -2,7 +2,6 @@ package br.com.trajy.architecture.controller.config;
 
 import br.com.trajy.architecture.assembly.AssemblyMapperInterface;
 import br.com.trajy.architecture.model.AuditableEntity;
-import br.com.trajy.architecture.resource.AuditableResource;
 import br.com.trajy.architecture.service.ServiceAbstract;
 
 public interface ControllerConfigInteface {
@@ -10,7 +9,7 @@ public interface ControllerConfigInteface {
     <ID_TYPE, ENTITY extends AuditableEntity<ID_TYPE>,
             SERVICE extends ServiceAbstract<ID_TYPE, ENTITY>> SERVICE getService();
 
-    <ID_TYPE, RESOURCE extends AuditableResource<ID_TYPE>, ENTITY extends AuditableEntity<ID_TYPE>,
+    <ID_TYPE, RESOURCE, ENTITY extends AuditableEntity<ID_TYPE>,
             ASSEMBLY extends AssemblyMapperInterface<RESOURCE, ENTITY>> ASSEMBLY getAssembly();
 
 }
