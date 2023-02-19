76414fc07e1a910e463092f11a84b60c75a48820
Trajy <henrique.trajano.a@gmail.com>
2023-02-19 16:33:02 -0300

    added-@Type-annotation-to-support-joda-date-time-persistence-with-hibernate

--- src/main/java/br/com/trajy/architecture/data/struct/model/AuditableEntity.java
+++ src/main/java/br/com/trajy/architecture/data/struct/model/AuditableEntity.java
@@ -3,7 +3,9 @@ package br.com.trajy.architecture.data.struct.model;
 import br.com.trajy.architecture.data.struct.common.Id;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
 import org.joda.time.DateTime;
+import org.joda.time.contrib.hibernate.PersistentDateTime;
 import javax.persistence.MappedSuperclass;
 
 @Getter
@@ -13,10 +15,12 @@ public abstract class AuditableEntity<T> extends Id<T> {
 
     private String cratedBy;
 
+    @Type(type = "org.joda.time.contrib.hibernate.PersistentDateTime")
     private DateTime createdAt;
 
     private String modifiedBy;
 
+    @Type(type = "org.joda.time.contrib.hibernate.PersistentDateTime")
     private DateTime modifiedAt;
 
 }
