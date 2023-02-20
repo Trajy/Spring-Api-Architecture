721f4f3a3421e17fff1dbf6ea8d6dd1799de4f38
Trajy <henrique.trajano.a@gmail.com>
2023-02-20 18:59:16 -0300

    created-exeption-handler-to-null-poiter-exception

--- src/main/java/br/com/trajy/architecture/restful/exception/RestExecptionHandler.java
+++ src/main/java/br/com/trajy/architecture/restful/exception/RestExecptionHandler.java
@@ -1,9 +1,32 @@
 package br.com.trajy.architecture.restful.exception;
 
+import static java.lang.String.valueOf;
+
+import br.com.trajy.architecture.restful.exception.data.struct.ErrorMessage;
+import br.com.trajy.architecture.restful.exception.data.struct.detail.ErrorDetail;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @ControllerAdvice
 public class RestExecptionHandler extends ResponseEntityExceptionHandler {
 
+    @ExceptionHandler(value = NullPointerException.class)
+    public ResponseEntity<ErrorMessage> nullPointerExeptionHandler(NullPointerException exception,
+                                               HttpServletRequest request, HttpServletResponse response) {
+        return ResponseEntity.status(response.getStatus())
+                .body(ErrorMessage.builder()
+                        .status(valueOf(response.getStatus()))
+                        .title(exception.getMessage())
+                        .type(request.getRequestURI())
+                        .detail(ErrorDetail.builder()
+                                    .error(exception.getCause().getMessage())
+                                    .build()
+                        )
+                        .build()
+                );
+    }
 }
