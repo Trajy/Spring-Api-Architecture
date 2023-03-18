cbb82bf68035c9b482085ae29874564f48446c5e
Trajy <henrique.trajano.a@gmail.com>
2023-03-18 19:10:41 -0300

    improve-generic-types-in-ControllerConfigInterface

--- src/main/java/br/com/trajy/architecture/layer/controller/config/ControllerConfigInteface.java
+++ src/main/java/br/com/trajy/architecture/layer/controller/config/ControllerConfigInteface.java
@@ -2,6 +2,7 @@ package br.com.trajy.architecture.layer.controller.config;
 
 import br.com.trajy.architecture.layer.assembly.AssemblyMapperInterface;
 import br.com.trajy.architecture.layer.data.struct.model.AuditableEntity;
+import br.com.trajy.architecture.layer.data.struct.resource.AuditableResource;
 import br.com.trajy.architecture.layer.service.ServiceAbstract;
 
 public interface ControllerConfigInteface {
@@ -9,6 +10,7 @@ public interface ControllerConfigInteface {
     <ID_TYPE, ENTITY extends AuditableEntity<ID_TYPE>,
             SERVICE extends ServiceAbstract<ID_TYPE, ENTITY>> SERVICE getService();
 
-    <RESOURCE, ENTITY, ASSEMBLY extends AssemblyMapperInterface<RESOURCE, ENTITY>> ASSEMBLY getAssembly();
+    <ID_TYPE, RESOURCE extends AuditableResource<ID_TYPE>, ENTITY extends AuditableEntity<ID_TYPE>,
+            ASSEMBLY extends AssemblyMapperInterface<RESOURCE, ENTITY>> ASSEMBLY getAssembly();
 
 }
