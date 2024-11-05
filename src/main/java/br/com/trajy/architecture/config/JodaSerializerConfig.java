b4fe543f5e354e0534212423f8c35fde48465778
Trajy <henrique.trajano.a@gmail.com>
2024-11-05 13:28:44 -0300

    remove serialization as timestamps in JodaSerializerConfig

--- src/main/java/br/com/trajy/architecture/config/JodaSerializerConfig.java
+++ src/main/java/br/com/trajy/architecture/config/JodaSerializerConfig.java
@@ -1,6 +1,5 @@
 package br.com.trajy.architecture.config;
 
-import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.joda.JodaModule;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +11,6 @@ public class JodaSerializerConfig {
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jacksonCustomizer() {
         return builder -> {
-            builder.featuresToEnable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
             builder.modules(new JodaModule());
         };
     }
