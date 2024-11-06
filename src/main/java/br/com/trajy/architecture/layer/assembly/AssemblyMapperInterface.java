14498ee1ae33185f7e29e92b4b390866507346d3
Trajy <henrique.trajano.a@gmail.com>
2024-11-06 08:09:48 -0300

    added return null if argument is null in methods for assembly collections

--- src/main/java/br/com/trajy/architecture/layer/assembly/AssemblyMapperInterface.java
+++ src/main/java/br/com/trajy/architecture/layer/assembly/AssemblyMapperInterface.java
@@ -1,5 +1,6 @@
 package br.com.trajy.architecture.layer.assembly;
 
+import static java.util.Objects.isNull;
 import static java.util.stream.Collectors.toList;
 
 import java.util.List;
@@ -11,10 +12,16 @@ public interface AssemblyMapperInterface<RESOURCE, ENTITY> {
     ENTITY toEntity(RESOURCE resource);
 
      default List<RESOURCE> toResource(List<ENTITY> entities) {
+         if(isNull(entities)) {
+             return null;
+         }
          return entities.stream().map(this::toResource).collect(toList());
      }
 
     default List<ENTITY> toEntity(List<RESOURCE> resources) {
+         if(isNull(resources)) {
+             return null;
+         }
          return resources.stream().map(this::toEntity).collect(toList());
     }
 
