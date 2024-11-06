e340ee63758cca58f477920379ff9e9c3cf457f8
Trajy <henrique.trajano.a@gmail.com>
2024-11-06 14:25:30 -0300

    created assemblyUtils class with methods to preventCiclicReferenceInDto

---
+++ src/main/java/br/com/trajy/architecture/layer/assembly/utils/AssemblyUtils.java
@@ -0,0 +1,41 @@
+package br.com.trajy.architecture.layer.assembly.utils;
+
+import static br.com.trajy.architecture.config.ApplicationContextStaticConfig.obtainContext;
+import static java.util.Objects.isNull;
+import static java.util.stream.Collectors.toList;
+import static org.apache.commons.collections4.CollectionUtils.isEmpty;
+
+import br.com.trajy.architecture.config.ApplicationContextStaticConfig;
+import br.com.trajy.architecture.layer.assembly.AssemblyMapperInterface;
+import org.springframework.context.annotation.Import;
+import java.util.List;
+import java.util.function.Consumer;
+
+@Import(ApplicationContextStaticConfig.class)
+public final class AssemblyUtils {
+
+    private AssemblyUtils() { }
+
+    public static <SOURCE, DESTINATION, A extends AssemblyMapperInterface> List<DESTINATION> preventCiclicReference(List<SOURCE> sources, Consumer<SOURCE> actionFunc, Class<A> assemblyClazz) {
+        if(isEmpty(sources)) {
+            return null;
+        }
+        A assembly = obtainContext().getBean(assemblyClazz);
+        return sources.stream()
+                .map(entity -> {
+                    actionFunc.accept(entity);
+                    return (DESTINATION) assembly.toResource(entity);
+                })
+                .collect(toList());
+    }
+
+    public static <SOURCE, DESTINATION, A extends AssemblyMapperInterface> DESTINATION preventCiclicReference(SOURCE source, Runnable actionFunc, Class<A> assemblyClazz) {
+        if(isNull(source)) {
+            return null;
+        }
+        A assembly = obtainContext().getBean(assemblyClazz);
+        actionFunc.run();
+        return (DESTINATION) assembly.toResource(source);
+    }
+
+}
