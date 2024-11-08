8d70a1fb7c4271d12098e59fadd602a49b47c6ac
Trajy <henrique.trajano.a@gmail.com>
2024-11-08 06:19:16 -0300

    added updatable = false flag into cratedBy and createdAt properties in AuditableEntity

--- src/main/java/br/com/trajy/architecture/layer/data/struct/model/AuditableEntity.java
+++ src/main/java/br/com/trajy/architecture/layer/data/struct/model/AuditableEntity.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.Type;
 import org.joda.time.DateTime;
+import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
@@ -12,9 +13,11 @@ import javax.persistence.MappedSuperclass;
 @Setter
 public abstract class AuditableEntity<T> extends Identity<T> {
 
+    @Column(updatable = false)
     private String createdBy;
 
     @Type(type = "org.jadira.usertype.dateandtime.joda.PersistentDateTime")
+    @Column(updatable = false)
     private DateTime createdAt;
 
     private String modifiedBy;
