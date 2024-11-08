03b4895ebed50a8a95d47eb959efa270d9a1162e
Trajy <henrique.trajano.a@gmail.com>
2024-11-08 06:22:14 -0300

    change checkNotNoll to checkArgument end checkState

--- src/main/java/br/com/trajy/architecture/layer/service/utils/ServiceUtils.java
+++ src/main/java/br/com/trajy/architecture/layer/service/utils/ServiceUtils.java
@@ -2,8 +2,11 @@ package br.com.trajy.architecture.layer.service.utils;
 
 
 import static br.com.trajy.architecture.config.ApplicationContextStaticConfig.obtainContext;
+import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
 import static java.lang.String.valueOf;
+import static java.util.Objects.nonNull;
 import static org.apache.commons.lang3.ClassUtils.getSimpleName;
 import static org.apache.commons.lang3.StringUtils.SPACE;
 
@@ -17,16 +20,18 @@ public final class ServiceUtils {
 
     public static <E extends AuditableEntity<Long>, S extends ServiceAbstract<Long, E>> E getIfExists(E entity, Class<E> entityClazz, Class<S> serviceClass) {
         String entityClazzName = getSimpleName(entityClazz);
-        checkNotNull(entity, entityClazzName.concat(SPACE).concat("entity com id deve ser informado"));
-        checkNotNull(entity.getId(), "id da entity".concat(SPACE).concat(entityClazzName).concat(SPACE).concat("deve ser informado"));
-        return checkNotNull(
-            obtainContext().getBean(serviceClass).findById(entity.getId()),
+        checkArgument(nonNull(entity), entityClazzName.concat(SPACE).concat("entity com id deve ser informado"));
+        checkArgument(nonNull(entity.getId()), "id da entity".concat(SPACE).concat(entityClazzName).concat(SPACE).concat("deve ser informado"));
+        E foundEntity = obtainContext().getBean(serviceClass).findById(entity.getId());
+        checkState(
+            nonNull(foundEntity),
             entity.getClass().getSimpleName()
                     .concat(SPACE)
                     .concat("não encontrado para o id:")
                     .concat(SPACE)
                     .concat(valueOf(entity.getId()))
         );
+        return foundEntity;
     }
 
 }
