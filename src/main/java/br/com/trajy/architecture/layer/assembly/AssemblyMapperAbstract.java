69e117a3b0e53503722a45b1d59aae4270f84853
Trajy <henrique.trajano.a@gmail.com>
2024-11-14 09:22:12 -0300

    move methods to handle collections implementations from AssemblyMapperInterface to AssemblyMapperAbstract

--- src/main/java/br/com/trajy/architecture/layer/assembly/AssemblyMapperAbstract.java
+++ src/main/java/br/com/trajy/architecture/layer/assembly/AssemblyMapperAbstract.java
@@ -1,20 +1,28 @@
 package br.com.trajy.architecture.layer.assembly;
 
 import static java.util.stream.Collectors.toList;
+import static org.apache.commons.collections4.CollectionUtils.isEmpty;
 import static org.apache.commons.collections4.ListUtils.emptyIfNull;
 
+import org.apache.commons.collections4.CollectionUtils;
 import java.util.List;
 
 public abstract class AssemblyMapperAbstract<RESOURCE, ENTITY> implements AssemblyMapperInterface<RESOURCE, ENTITY> {
 
     @Override
     public List<RESOURCE> toResource(List<ENTITY> entities) {
-        return emptyIfNull(entities.stream().map(this::toResource).collect(toList()));
+        if(isEmpty(entities)) {
+            return null;
+        }
+        return entities.stream().map(this::toResource).collect(toList());
     }
 
     @Override
     public List<ENTITY> toEntity(List<RESOURCE> resources) {
-        return emptyIfNull(resources.stream().map(this::toEntity).collect(toList()));
+        if(isEmpty(resources)) {
+            return null;
+        }
+        return resources.stream().map(this::toEntity).collect(toList()));
     }
 
 }
--- src/main/java/br/com/trajy/architecture/layer/assembly/AssemblyMapperInterface.java
+++ src/main/java/br/com/trajy/architecture/layer/assembly/AssemblyMapperInterface.java
@@ -1,8 +1,5 @@
 package br.com.trajy.architecture.layer.assembly;
 
-import static java.util.Objects.isNull;
-import static java.util.stream.Collectors.toList;
-
 import java.util.List;
 
 public interface AssemblyMapperInterface<RESOURCE, ENTITY> {
@@ -11,18 +8,8 @@ public interface AssemblyMapperInterface<RESOURCE, ENTITY> {
 
     ENTITY toEntity(RESOURCE resource);
 
-     default List<RESOURCE> toResource(List<ENTITY> entities) {
-         if(isNull(entities)) {
-             return null;
-         }
-         return entities.stream().map(this::toResource).collect(toList());
-     }
+    List<RESOURCE> toResource(List<ENTITY> entities);
 
-    default List<ENTITY> toEntity(List<RESOURCE> resources) {
-         if(isNull(resources)) {
-             return null;
-         }
-         return resources.stream().map(this::toEntity).collect(toList());
-    }
+    List<ENTITY> toEntity(List<RESOURCE> resources);
 
 }
