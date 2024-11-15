1854398e64e876a6b224a5557fc7a1d4f9ccbc0e
Trajy <henrique.trajano.a@gmail.com>
2024-11-15 15:41:43 -0300

    fix string format into FindByIdService

--- src/main/java/br/com/trajy/architecture/layer/service/FindByIdService.java
+++ src/main/java/br/com/trajy/architecture/layer/service/FindByIdService.java
@@ -14,7 +14,7 @@ public interface FindByIdService<ID_TYPE, ENTITY extends AuditableEntity<ID_TYPE
     default ENTITY findById(ID_TYPE id) {
         this.beforeFind(id);
         ENTITY entity = this.getRepository().findById(id)
-                .orElseThrow(() -> new IllegalArgumentException(format("Entity no found for id: %f", id)));
+                .orElseThrow(() -> new IllegalArgumentException(format("Entity no found for id: %d", id)));
         this.afterFind(id, entity);
         return entity;
     }
